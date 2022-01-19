1cf457a80153eee626dbc54757f261b6dd2a4c0d
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-01-19 08:57:23 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -3,15 +3,25 @@ document.getElementById('modeBtn').addEventListener('click', function () {
   // document.getElementById('header').classList.toggle('dark');
   document.body.classList.toggle('dark');
   if (a == 0) {
-    this.textContent = 'Li';
     a++;
   } else {
-    this.textContent = 'Da';
     a = 0;
   }
 });
 
+let modalBtn = document.getElementById("openBtn");
+let modal = document.getElementById("openModal")
+let closeModal = document.getElementById("closeBtn")
+let bodyPadding = document.getElementById("bodyPadding")
 
+modalBtn.addEventListener('click',function(){
+  modal.style.display = "block"
+  bodyPadding.style.paddingTop = "0"
+  
+} )
+closeModal.addEventListener('click', function(){
+  modal.style.display = 'none'
+})
 
 
 
